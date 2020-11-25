4fe7fe8e06c33d4a5414c64404baf3bd6764de53
vdmkbu <vdmkbu@ya.ru>
2020-11-25 16:57:18 +0500

    внешний ключ user_id для Company

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'INN',
+        'file',
+        'OKVED',
+        'date',
+        'user_id'
+    ];
+
+    public function scopeOwner($query, $user_id)
+    {
+        $query->where('user_id', $user_id);
+    }
 }
