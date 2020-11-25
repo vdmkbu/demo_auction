4f2196279ffbb192011993e114dd34159fa8a188
vdmkbu <vdmkbu@ya.ru>
2020-11-25 18:26:21 +0500

    Шаблон

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,19 +5,101 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('font-awesome/css/font-awesome.css') }}" rel="stylesheet">
 
+    <!-- Toastr style -->
+    <link href="{{ asset('css/plugins/toastr/toastr.min.css') }}" rel="stylesheet">
+
+    <!-- Gritter -->
+    <link href="{{ asset('js/plugins/gritter/jquery.gritter.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/plugins/dataTables/datatables.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/plugins/codemirror/codemirror.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/plugins/codemirror/ambiance.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/plugins/blueimp/css/blueimp-gallery.min.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('css/plugins/jasny/jasny-bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/plugins/iCheck/custom.css') }}" rel="stylesheet">
     <link href="{{ asset('css/animate.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('css/plugins/awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css') }}" rel="stylesheet">
+
 
 </head>
 
 <body class="gray-bg">
+<div id="wrapper" >
     @include('layouts.partials.flash')
     @yield('content')
+</div>
 
+<!-- Mainly scripts -->
+<script src="{{ asset('js/jquery-3.1.1.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/plugins/metisMenu/jquery.metisMenu.js') }}"></script>
+<script src="{{ asset('js/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js"></script>
+
+<!-- Flot -->
+<script src="{{ asset('js/plugins/flot/jquery.flot.js') }}"></script>
+<script src="{{ asset('js/plugins/flot/jquery.flot.tooltip.min.js') }}"></script>
+<script src="{{ asset('js/plugins/flot/jquery.flot.spline.js') }}"></script>
+<script src="{{ asset('js/plugins/flot/jquery.flot.resize.js') }}"></script>
+<script src="{{ asset('js/plugins/flot/jquery.flot.pie.js') }}"></script>
+
+<!-- Peity -->
+<script src=" {{ asset('js/plugins/peity/jquery.peity.min.js') }}"></script>
+<script src=" {{ asset('js/demo/peity-demo.js') }}"></script>
+
+
+<script src="{{ asset('js/plugins/pace/pace.min.js') }}"></script>
+
+<!-- jQuery UI -->
+<script src="{{ asset('js/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+
+<!-- GITTER -->
+<script src="{{ asset('js/plugins/gritter/jquery.gritter.min.js') }}"></script>
+
+<!-- Sparkline -->
+<script src="{{ asset('js/plugins/sparkline/jquery.sparkline.min.js') }}"></script>
+
+<!-- blueimp gallery -->
+<script src="{{ asset('js/plugins/blueimp/jquery.blueimp-gallery.min.js') }}"></script>
+
+<!-- Sparkline demo data  -->
+<script src="{{ asset('js/demo/sparkline-demo.js') }}"></script>
+
+<!-- ChartJS-->
+<script src="{{ asset('js/plugins/chartJs/Chart.min.js') }}"></script>
+
+<!-- Toastr -->
+<script src="{{ asset('js/plugins/toastr/toastr.min.js') }}"></script>
+
+<script src="{{ asset('js/plugins/metisMenu/jquery.metisMenu.js') }}"></script>
+<script src="{{ asset('js/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
+<script src="{{ asset('js/plugins/dataTables/datatables.min.js') }}"></script>
+
+<!-- Jasny -->
+<script src="{{ asset('js/plugins/jasny/jasny-bootstrap.min.js') }}"></script>
+
+<script src="{{ asset('js/plugins/iCheck/icheck.min.js') }}"></script>
+<script>
+    $(document).ready(function () {
+        $('.i-checks').iCheck({
+            checkboxClass: 'icheckbox_square-green',
+            radioClass: 'iradio_square-green',
+        });
+
+        $('.i-checks-nds').iCheck({
+            checkboxClass: 'icheckbox_square-green',
+            radioClass: 'iradio_square-green',
+        });
+    });
+</script>
+
+<!-- Custom and plugin javascript -->
+<script src="/js/inspinia.js?<?=filemtime($_SERVER['DOCUMENT_ROOT'].'/js/inspinia.js')?>"></script>
 </body>
 </html>
--- resources/views/layouts/inner.blade.php
+++ resources/views/layouts/inner.blade.php
@@ -1,5 +1,80 @@
 @extends('layouts.app')
 
+<!-- меню слева -->
+@section('content')
+<nav class="navbar-default navbar-static-side" role="navigation">
+    <div class="sidebar-collapse">
+        <ul class="nav metismenu" id="side-menu">
+            <li class="nav-header">
+                <div class="dropdown profile-element"> <span></span>
+                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                                <span class="clear">
+
+                                    <span class="block m-t-xs"><strong class="font-bold">Login</strong> <b class="caret"></b></span>
+                                 </span>
+                    </a>
+                    <ul class="dropdown-menu animated fadeInRight m-t-xs">
+                        <li><a href="/profile">Профиль</a></li>
+
+                        <li class="divider"></li>
+                        <li><a href="">Выход</a></li>
+                    </ul>
+
+
+                </div>
+
+                <span class="text-info">Баланс: Account р. </span>
+                <span class="text-info">Свободные: ['Account']-$reserved р.</span>
+                <span class="text-info">Резерв: $reserved р.</span>
+                <div class="logo-element">
+                    b-con
+                </div>
+            </li>
+
+            <!-- меню -->
+
+            <li class="navbar-default"><a href=""><i class="fa fa-list"></i> <span class="nav-label">$sd->name</span></a></li>
+
+
+            <li class="navbar-default"><a href="/bids/"><i class="fa fa-list"></i> <span class="nav-label">Логи ставок (видит админ)</span></a></li>
+
+
+        </ul>
+
+    </div>
+</nav>
+
+<div id="page-wrapper" class="gray-bg dashbard-1">
+
+    <!-- хедер -->
+    <div class="row border-bottom">
+        <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
+                <form role="search" class="navbar-form-custom" action="search_results.html">
+                    <div class="form-group">
+                        <input type="text" placeholder="Поиск по сайту..." class="form-control" name="top-search" id="top-search">
+                    </div>
+                </form>
+            </div>
+            <ul class="nav navbar-top-links navbar-right">
+                <li>
+                    <span class="m-r-sm text-muted welcome-message">Добро пожаловать</span>
+                </li>
+
+                <li>
+                    <a href="">
+                        <i class="fa fa-sign-out"></i> Выход
+                    </a>
+                </li>
+                <li>
+                    <a class="right-sidebar-toggle">
+                        <i class="fa fa-tasks"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div> <!-- row border-bottom -->
 
 <div class="row wrapper border-bottom white-bg page-heading">
     <div class="col-lg-10">
@@ -15,3 +90,5 @@
     @yield('content_inner')
 </div>
 
+</div>
+@endsection
