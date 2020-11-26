27ac896f557dcfff24355d160f9f6df1e1cc43d3
vdmkbu <vdmkbu@ya.ru>
2020-11-26 19:45:20 +0500

    модель и миграции News

---
+++ database/migrations/2020_11_26_143643_create_news_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id()->unsigned();
+            $table->timestamps();
+            $table->string('name');
+            $table->dateTime('date');
+            $table->text('announce');
+            $table->text('text');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
