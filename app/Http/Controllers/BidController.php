a8b2e70a7520e8cbf4a5b274fdcc304e6bd9f944
vdmkbu <vdmkbu@ya.ru>
2020-12-07 20:19:35 +0500

    рефакторинг BidController

--- app/Http/Controllers/BidController.php
+++ app/Http/Controllers/BidController.php
@@ -6,47 +6,29 @@ use App\Events\LotReceivedNewBid;
 use App\Models\Bid;
 use App\Models\Lot;
 use App\Models\User;
+use App\Repositories\UserRepository;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class BidController extends Controller
 {
+    private UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     public function store(Lot $lot, Request $request)
     {
 
         // еще раз проверим, что можем ставить ставку
 
         $user = User::find(auth()->id());
-        $account = $user->account;
+        $account = $this->userRepository->getUserAccount($user);
 
         // все деньги, которые зарезервированы под уже поставленные максимальные ставки
-        $reserved = [];
-
-        // получим все lot_id из ставок
-        $lot_ids = DB::table('bids')->select('lot_id')->groupBy('lot_id')->get();
-        foreach ($lot_ids as $lot_data) {
-            $lot_id = $lot_data->lot_id;
-
-            // для каждого лота получим максимальное значение ставки и id пользователя, который её поставил
-            $data = DB::table('bids')->select(['user_id', 'bid'])->where('lot_id','=',$lot_id)->orderBy('bid', 'DESC')->limit(1)->get();
-
-            foreach ($data as $item) {
-                $user_id = $item->user_id;
-                $bid = $item->bid;
-
-                // если макс. ставка у текущего пользователя, то считаем сумму
-                if ($user_id == auth()->id()) {
-
-                    $sum = Lot::find($lot_id)->sum;
-                    $reserved[] = ($sum/100) * $bid;
-
-                }
-
-            }
-        }
-
-        $reserved = array_sum($reserved);
-        $free = $account - $reserved;
+        $reserved = $this->userRepository->getReservedMoney($user);
+        $free = $this->userRepository->getFreeMoney($account, $reserved);
 
         $sum = $lot->sum;
         $comission = ($sum/100) * $request->Bid;
