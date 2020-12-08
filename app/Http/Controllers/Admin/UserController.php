f6a823ea5be003f049d73a7539b4492a99a6ba0a
vdmkbu <vdmkbu@ya.ru>
2020-12-08 12:57:16 +0500

    сортировка пользователей в админке

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        $users = User::orderBy('id', 'ASC')->get();
         return view('admin.users.index', compact('users'));
     }
 
